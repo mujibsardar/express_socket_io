2bfe824c319abfe6b339e30eb34d7f7368201749
Avan <mujibsardar@gmail.com>
2019-05-21 14:07:40 -0700

    Initial commit

---
+++ app/database/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var config 		= require('../config');
+var Mongoose 	= require('mongoose');
+var logger 		= require('../logger');
+
+// Connect to the database
+// construct the database URI and encode username and password.
+// mongodb+srv://avan:<password>@cluster0-4x5s3.mongodb.net/test?retryWrites=true
+var dbURI = "mongodb+srv://" +
+			encodeURIComponent(config.db.username) + ":" +
+			encodeURIComponent(config.db.password) + "@" +
+			// config.db.host + ":" +
+			config.db.host + "/" +
+			// config.db.port + "/" +
+			config.db.name + "?" +
+			"retryWrites=true"
+
+Mongoose.connect(dbURI, { useNewUrlParser: true });
+
+// Throw an error if the connection fails
+Mongoose.connection.on('error', function(err) {
+	if(err) throw err;
+});
+
+// mpromise (mongoose's default promise library) is deprecated,
+// Plug-in your own promise library instead.
+// Use native promises
+Mongoose.Promise = global.Promise;
+
+module.exports = { Mongoose,
+	models: {
+		user: require('./schemas/user.js'),
+		room: require('./schemas/room.js')
+	}
+};
+
+// brew services start redis
+// redis-server /usr/local/etc/redis.conf
